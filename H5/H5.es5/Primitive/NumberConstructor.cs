e4d9b006b212b78abeba70fb005220e86115e289
rafael-aero <8791811+rafael-aero@users.noreply.github.com>
2020-05-08 22:53:06 +0200

    Revert "merge dom, es5 and core into one project"
    This reverts commit 35f41207818b59430aef8679b2c3bb403314019b.

---
+++ H5/H5.es5/Primitive/NumberConstructor.cs
@@ -0,0 +1,40 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: H5.Primitive.NumberConstructor
+// Assembly: H5.es5, Version=2.8.2.0, Culture=neutral, PublicKeyToken=null
+// MVID: EC57AC2B-0E02-4A1C-B567-F790F377783B
+// Assembly location: C:\work\curiosity\tesserae\Tesserae\bin\Debug\net461\H5.es5.dll
+
+using HighFive;
+
+namespace H5.Primitive
+{
+    [IgnoreCast]
+    [Namespace(false)]
+    [Virtual]
+    public interface NumberConstructor : IObject
+    {
+        [Template("new {this}()")]
+        Number New();
+
+        [Template("new {this}({0})")]
+        Number New(object value);
+
+        [Template("{this}()")]
+        double Self();
+
+        [Template("{this}({0})")]
+        double Self(object value);
+
+        Number prototype { get; }
+
+        double MAX_VALUE { get; }
+
+        double MIN_VALUE { get; }
+
+        double NaN { get; }
+
+        double NEGATIVE_INFINITY { get; }
+
+        double POSITIVE_INFINITY { get; }
+    }
+}

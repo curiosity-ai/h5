b3c1cedff3f2c375c757a8305845acad09779e51
rafael-aero <8791811+rafael-aero@users.noreply.github.com>
2020-05-10 10:55:02 +0200

    fix project build for base library

--- H5/H5/Resources/.generated/h5.js
+++ H5/H5/Resources/.generated/h5.js
@@ -3531,7 +3531,7 @@
 
     H5.init(function () {
         H5.SystemAssembly.version = "";
-        H5.SystemAssembly.compiler = "1.0.0";
+        H5.SystemAssembly.compiler = "0.0.7689";
     });
 
     H5.define("H5.Utils.SystemAssemblyVersion");

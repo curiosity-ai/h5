3d12f3d014892857bff2f43aee91e8200c02cf31
theolivenbaum <rafael@rafael.aero>
2020-05-12 20:31:27 +0200

    Update Header.js

--- H5/H5/Resources/.auxiliary/Header.js
+++ H5/H5/Resources/.auxiliary/Header.js
@@ -2,5 +2,5 @@
  * @version   : {version} - H5.NET
  * @author    : Object.NET, Inc. http://h5.net/
  * @copyright : Copyright 2008-{year} Object.NET, Inc. http://object.net/
- * @license   : See license.txt and https://github.com/curiosity-ai/h5/blob/master/LICENSE.md
+ * @license   : See license.txt and https://github.com/bridgedotnet/Bridge.NET/blob/master/LICENSE.
  */

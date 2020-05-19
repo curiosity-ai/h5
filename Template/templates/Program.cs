a940c30f9a2408f233ae2f40bc042ed30eab241a
rafael-aero <rafael@rafael.aero>
2020-05-19 10:15:58 +0200

    attempt fix SDK target project resolution

---
+++ Template/templates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using H5;
+using H5.Core;
+using static H5.Core.es5;
+using static H5.Core.dom;
+using Tesserae;
+using static Tesserae.UI;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = TextBlock("Hello world!");
+
+            document.body.appendChild(hello.Render());
+        }
+    }
+}

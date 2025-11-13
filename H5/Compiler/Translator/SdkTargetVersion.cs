8084fceae26f1991d9b4084e7473240276c6035e
theolivenbaum <rafael@curiosity.ai>
2025-11-13 15:07:23 +0100

    Update SdkTargetVersion.cs

--- H5/Compiler/Translator/SdkTargetVersion.cs
+++ H5/Compiler/Translator/SdkTargetVersion.cs
@@ -7,6 +7,6 @@ namespace H5.Translator
 {
     public static class SdkTargetVersion
     {
-        public static readonly NuGetVersion Latest = NuGetVersion.Parse("23.2.35852");
+        public static readonly NuGetVersion Latest = NuGetVersion.Parse("25.11.62684");
     }
 }
